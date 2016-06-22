b512d57698661e5cc4bdfd3e35ed85ac85b3a6e8
CHANG-YU-CHENG <nopony2352676@gmail.com>
2016-06-22 21:03:48 +0800

    add notification plugin folder

---
+++ Assets/GameFunctionScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameFunctionScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ShowNotification(){
+		using (AndroidJavaClass unity = new AndroidJavaClass("com.example.diclab_cheng.unity_notification_plugin.LocalNotificationActivity"))
+		{
+			unity.CallStatic("Show", 5, "這是標題", "這是內文", "black_heart96");
+		}
+	}
+}
